3daffe65c9b143c174ca72c6b78d180b7dd8658b
Christine Wei <christine411995@gmail.com>
2023-02-11 19:51:39 +0800

    add in connection to db and fetchone information in oauth2.get_current_user

--- app/routers/post.py
+++ app/routers/post.py
@@ -21,14 +21,14 @@ def get_posts(db: Session = Depends(get_db)):
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     ## SQL
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) 
     #                   RETURNING * """, 
     #                 (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
-    print(user_id)
+    print(current_user.email)
     ## ORM
     new_post = models.Post(**post.dict())
     db.add(new_post)
@@ -37,13 +37,13 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_i
     return new_post
 
 @router.get("/{id}")
-def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     ## SQL
     # cursor.execute("""SELECT * FROM posts WHERE id = %s;""", str(id))
     # post = cursor.fetchone()
     # print(post)
     # if post does not exist
-    print(user_id)
+    print(current_user.email)
     ## ORM
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
@@ -55,7 +55,7 @@ def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oaut
     return post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     ## SQL
     # cursor.execute("""DELETE FROM posts WHERE id = %s 
     #                 RETURNING *""", str(id))
@@ -74,7 +74,7 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(o
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post:schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def update_post(id: int, updated_post:schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     ## SQL
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s
     #                   WHERE id = %s
